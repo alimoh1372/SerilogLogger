bfeea5f4331a9b8c155a836a4f6e45e9e88abc11
A.Mohammadzadeh <A.Mohammadzadeh@hafez.com>
2024-06-18 15:56:28 +0330

    Initial commit

---
+++ SerilogLogger/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SerilogLogger.Dtos;
+using SerilogLogger.LoggerImplementation.LogToQueue;
+using SerilogLogger.LoggerImplementation.NormalLog;
+using SerilogLogger.LoggerInterface;
+
+namespace SerilogLogger;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddLoggerDependencies(this IServiceCollection services, ApplicationLogConfiguration logConfiguration)
+    {
+        if (logConfiguration.IsLogToQueue)
+            services.AddSingleton<ILog, SeriLogQueueLogger>(_=> new SeriLogQueueLogger(logConfiguration.ApplicationId,logConfiguration.ApplicationName));
+        else
+            services.AddSingleton<ILog, SeriLogNormalLogger>(_ => new SeriLogNormalLogger(logConfiguration.ApplicationId, logConfiguration.ApplicationName));
+        
+        return services;
+    }
+}
